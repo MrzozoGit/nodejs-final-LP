56d038458ea56466a00cf033c68f04c43fca1c94
Enzo Bassot <enzo.bassot@gmail.com>
2022-03-31 18:02:15 +0200

    projet nodejs

---
+++ server/plugins/swagger.js
@@ -0,0 +1,35 @@
+// Mettrer "Bearer [token]" sur l'application pour se connecter
+
+'use strict';
+
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const HapiSwagger = require('hapi-swagger');
+const Package = require('../../package.json');
+
+module.exports = {
+    name: 'app-swagger',
+    async register(server) {
+
+        await server.register([
+            Inert,
+            Vision,
+            {
+                plugin: HapiSwagger,
+                options: {
+                    info: {
+                        version: Package.version
+                    },
+                    securityDefinitions : {
+                        'jwt' : {
+                            'type' : 'apiKey',
+                            'name' : 'Authorization',
+                            'in'   : 'header'
+                        }
+                    },
+                    security            : [{ 'jwt' : [] }],
+                }
+            }
+        ]);
+    }
+};
